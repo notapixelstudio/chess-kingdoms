aa03ab79809337d696d5a48702c74635cebbbfc9
salvob <tholalak@yahoo.it>
2018-04-08 19:44:11 +0200

    added tileset chess-board and activated plugins

---
+++ assets/chess-board.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="chess-board" tilewidth="64" tileheight="64" tilecount="12" columns="4">
+ <image source="glass.png" width="256" height="192"/>
+</tileset>
