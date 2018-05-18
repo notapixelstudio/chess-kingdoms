7148223a3a7d82e3b1aa1117e24d70eef5488c3e
salvob <tholalak@yahoo.it>
2018-05-18 15:41:59 +0200

    small fix to card. If selected we don't want the bouncing stuff

--- actors/cards/card.gd
+++ actors/cards/card.gd
@@ -8,24 +8,30 @@ var piece_name = "shogi_pawn"
 var kingdom = "ruby"
 
 func _ready():
+	piece_name = "shogi_pawn"
+	kingdom = "ruby"
 	pass
 
 func _on_Character_mouse_entered():
 	focused = true
-	$AnimationPlayer.queue("focus")
+	if not selected:
+		$AnimationPlayer.queue("focus")
+
 	
 	
 func _on_Piece_mouse_exited():
 	focused = false
-	$AnimationPlayer.queue("unfocus")
+	if not selected:
+		$AnimationPlayer.queue("unfocus")
 
 
 
 func _on_Card_input_event(viewport, event, shape_idx):
 	if event.is_class("InputEventMouseButton") \
     and event.button_index == BUTTON_LEFT \
-    and event.pressed:
+    and event.pressed and not selected:
 		print("Clicked")
 		selected = true
 		model.selected_card = self
+		$AnimationPlayer.queue("unfocus")
 		return(self) # returns a reference to this node
